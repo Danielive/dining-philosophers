b22d481e3bbbccb2ebd44e0f190c47158c24df36
Daniel <Danielive@outlook.com>
2018-11-17 03:56:44 +0300

    fun-p-update

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,5 +1,8 @@
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -7,8 +10,10 @@ import java.util.List;
  */
 final class Manager {
 
-    private static List<Philosopher> philosophers = new ArrayList<>();
-    private static List<Fork> forks = new ArrayList<>();
+    private static List<Philosopher> philosophers = Collections.synchronizedList(new ArrayList<>());
+    private static List<Fork> forks = Collections.synchronizedList(new ArrayList<>());
+
+    static boolean stateDined = false;
 
     void execute(int count) {
         System.out.println("Number of philosophers: " + count);
@@ -21,15 +26,153 @@ final class Manager {
 
 
         while (true) {
-            getPhilosophers().stream().parallel().filter(this::choicePhilosopher).findAny().ifPresent(Philosopher::startDining);
+            stateDined = false;
+
+            for (Philosopher phil : Manager.getPhilosophers()) {
+                phil.setDined(false);
+                phil.setState(false);
+            }
+            for (Fork fork : Manager.getForks()) {
+                fork.setState(true);
+            }
+
+            for (Philosopher phil : Manager.getPhilosophers()) {
+                if ((phil.getNumber() + 1) < Manager.getPhilosophers().size())
+                    System.out.printf("Numb %s, state1 %s state2 %s, dined1 %s dined2 %s, fork1 %s fork2 %s%n",
+                            phil.getNumber() + 1,
+                            phil.getState(),
+                            philosophers.get(phil.getNumber() + 1).getState(),
+                            phil.getDined(),
+                            philosophers.get(phil.getNumber() + 1).getDined(),
+                            forks.get(phil.getNumber()).isState(),
+                            forks.get(phil.getNumber() + 1).isState()
+                    );
+                else System.out.printf("Numb %s, state1 %s state2 %s, dined1 %s dined2 %s, fork1 %s fork2 %s%n",
+                        phil.getNumber() + 1,
+                        phil.getState(),
+                        philosophers.get(0).getState(),
+                        phil.getDined(),
+                        philosophers.get(0).getDined(),
+                        forks.get(phil.getNumber()).isState(),
+                        forks.get(0).isState()
+                );
+            }
+
+            System.out.println("NEW!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+            int c = 0;
+            while (!stateDined) {
+                getPhilosophers()
+                        .stream()
+                        .filter(this::choicePhilosopher)
+                        .parallel()
+                        .forEach(Philosopher::dining);
+
+                int i = 0;
+                for (Philosopher phil : Manager.getPhilosophers()) {
+                    if (phil.getDined()) {
+                        System.out.println("NAME " + (phil.getNumber()+1));
+                        i++;
+                    }
+
+                    if (phil.getNumber()+1 < Manager.getPhilosophers().size())
+                        restore(phil.getNumber(), phil.getNumber()+1);
+                    else
+                        restore(phil.getNumber(), 0);
+
+//                    if ((phil.getNumber()+1) < Manager.getPhilosophers().size())
+//                        System.out.printf("Numb %s, state1 %s state2 %s, dined1 %s dined2 %s, fork1 %s fork2 %s%n",
+//                                phil.getNumber()+1,
+//                                phil.getState(),
+//                                philosophers.get(phil.getNumber()+1).getState(),
+//                                phil.getDined(),
+//                                philosophers.get(phil.getNumber()+1).getDined(),
+//                                forks.get(phil.getNumber()).isState(),
+//                                forks.get(phil.getNumber()+1).isState()
+//                        );
+//                    else System.out.printf("Numb %s, state1 %s state2 %s, dined1 %s dined2 %s, fork1 %s fork2 %s%n",
+//                            phil.getNumber()+1,
+//                            phil.getState(),
+//                            philosophers.get(0).getState(),
+//                            phil.getDined(),
+//                            philosophers.get(0).getDined(),
+//                            forks.get(phil.getNumber()).isState(),
+//                            forks.get(0).isState()
+//                    );
+                }
+
+                System.out.println(i + " + " + count);
+                //System.out.println(i);
+                if (i == count) stateDined = true;
+
+                for (Philosopher phil : Manager.getPhilosophers()) {
+                    if ((phil.getNumber() + 1) < Manager.getPhilosophers().size())
+                        System.out.printf("Numb %s, state1 %s state2 %s, dined1 %s dined2 %s, fork1 %s fork2 %s%n",
+                                phil.getNumber() + 1,
+                                phil.getState(),
+                                philosophers.get(phil.getNumber() + 1).getState(),
+                                phil.getDined(),
+                                philosophers.get(phil.getNumber() + 1).getDined(),
+                                forks.get(phil.getNumber()).isState(),
+                                forks.get(phil.getNumber() + 1).isState()
+                        );
+                    else System.out.printf("Numb %s, state1 %s state2 %s, dined1 %s dined2 %s, fork1 %s fork2 %s%n",
+                            phil.getNumber() + 1,
+                            phil.getState(),
+                            philosophers.get(0).getState(),
+                            phil.getDined(),
+                            philosophers.get(0).getDined(),
+                            forks.get(phil.getNumber()).isState(),
+                            forks.get(0).isState()
+                    );
+                }
+
+                c++;
+                if (c==10) break;
+            }
+            if (c==10) break;
         }
     }
+
+    private StatePhilosopher state = (one, two) -> {
+        if (Manager.getForks().get(one).isState() && Manager.getForks().get(two).isState() &&
+        !(Manager.getPhilosophers().get(one).getState() && Manager.getPhilosophers().get(two).getState()) &&
+                !Manager.getPhilosophers().get(one).getDined()) {
+            Manager.getForks().get(one).setState(false);
+            Manager.getForks().get(two).setState(false);
+            Manager.getPhilosophers().get(one).setState(true);
+            Manager.getPhilosophers().get(two).setState(true);
+            System.out.println("DO NUM 1 - " + (one+1) + " NUM 2 - " + (two+1));
+            return true;
+        }
+        return false;
+    };
+
+    @NotNull
+    private Boolean restore(Integer one, Integer two) {
+        if (!(Manager.getForks().get(one).isState() && Manager.getForks().get(two).isState()) &&
+                (Manager.getPhilosophers().get(one).getState() && Manager.getPhilosophers().get(two).getState()) &&
+                Manager.getPhilosophers().get(one).getDined()) {
+            Manager.getForks().get(one).setState(true);
+            Manager.getForks().get(two).setState(true);
+            Manager.getPhilosophers().get(one).setState(false);
+            Manager.getPhilosophers().get(two).setState(false);
+
+            System.out.println("RESTORE NUM 1 - " + (one+1) + " NUM 2 - " + (two+1));
+            //Manager.getPhilosophers().get(one).setDined(true);
+            return true;
+        }
+        return false;
+    }
+
+    @Contract(pure = true)
+    private Boolean isState(Integer oneFork, Integer twoFork) {return state.isReady(oneFork, twoFork);}
 
     @Contract(pure = true)
     private Boolean choicePhilosopher(Philosopher philosopher) {
         if ((philosopher.getNumber()+1) < Manager.getPhilosophers().size())
-            return philosopher.isState(philosopher.getNumber(), (philosopher.getNumber() + 1));
-        else return philosopher.isState(philosopher.getNumber(), 0);
+            return isState(philosopher.getNumber(), (philosopher.getNumber() + 1));
+        else return isState(philosopher.getNumber(), 0);
     }
 
     @Contract(pure = true)
--- src/main/java/Philosopher.java
+++ src/main/java/Philosopher.java
@@ -11,45 +11,85 @@ final class Philosopher {
     final private static SimpleDateFormat formatDate = new SimpleDateFormat("mm:ss:SSS");
     final private Integer number;
     private String name;
+    private Boolean state;
+    private Boolean dined;
 
     Philosopher(final int numPhilosopher) {
         setName("Philosopher-" + (numPhilosopher+1));
         number = numPhilosopher;
+        state = false;
         System.out.println(getName() + " has sit at the table : " + formatDate.format(new Date()));
+        dined = false;
     }
 
-    void startDining() {
-        if ((number+1) < Manager.getForks().size()) {
-            dining(number, (number + 1));
-        }
-        else {
-            dining(number, 0);
+//    void startDining() {
+//        if (((number+1) < Manager.getForks().size()) && !Manager.getPhilosophers().get(number).getState() && !Manager.getPhilosophers().get(number+1).getState()) {
+//            dining(number, (number + 1));
+//        }
+//        else if (!Manager.getPhilosophers().get(number).getState() && !Manager.getPhilosophers().get(0).getState()){
+//            dining(number, 0);
+//        }
+//    }
+
+
+    void dining() {
+        if (!getDined() && getState()) {
+            //Manager.getPhilosophers().get(number).setState(true);
+
+            if ((number + 1) < Manager.getForks().size())
+                System.out.println(getName() + " takes forks " + (number + 1) + " and " + (number + 2) + " : " + Philosopher.getFormatDate().format(new Date()));
+            else
+                System.out.println(getName() + " takes forks " + (number + 1) + " and " + (1) + " : " + Philosopher.getFormatDate().format(new Date()));
+
+            System.out.println(getName() + " begin dining : " + Philosopher.getFormatDate().format(new Date()));
+
+//        Manager.getForks().get(oneFork).setState(false);
+//        Manager.getForks().get(twoFork).setState(false);
+
+            // dining
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+
+            System.out.println(getName() + " end dining : " + Philosopher.getFormatDate().format(new Date()));
+
+            //Manager.getForks().get(number).setState(true);
+            //Manager.getForks().get(number+1).setState(true);
+
+            //Manager.getPhilosophers().get(number).setState(false);
+            //Manager.getPhilosophers().get(number+1).setState(false);
+
+            setDined(true);
         }
     }
+//
+//    private StatePhilosopher state = (oneFork, twoFork) -> {
+//        if (Manager.getForks().get(oneFork).isState() && Manager.getForks().get(twoFork).isState()) {
+//            Manager.getForks().get(oneFork).setState(false);
+//            Manager.getForks().get(twoFork).setState(false);
+//            return true;
+//        }
+//        return false;
+//    };
 
-    private void dining(int oneFork, int twoFork) {
-        System.out.println(Manager.getPhilosophers().get(oneFork).getName() + " takes forks " + (oneFork + 1) + " and " + (twoFork + 1) + " : " + Philosopher.getFormatDate().format(new Date()));
-        System.out.println(Manager.getPhilosophers().get(oneFork).getName() + " begin dining : " + Philosopher.getFormatDate().format(new Date()));
 
-        Manager.getForks().get(oneFork).setState(false);
-        Manager.getForks().get(twoFork).setState(false);
-
-        // dining
-        try {
-            Thread.sleep(1000);
-        }
-        catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-
-        System.out.println(Manager.getPhilosophers().get(oneFork).getName() + " end dining : " + Philosopher.getFormatDate().format(new Date()));
-
-        Manager.getForks().get(oneFork).setState(true);
-        Manager.getForks().get(twoFork).setState(true);
+    @Contract(pure = true)
+    Boolean getDined() {
+        return dined;
+    }
+    public void setDined(Boolean dined) {
+        this.dined = dined;
     }
 
-    private StatePhilosopher state = (oneFork, twoFork) ->
-            Manager.getForks().get(oneFork).isState() && Manager.getForks().get(twoFork).isState();
+    @Contract(pure = true)
+    public Boolean getState() {
+        return state;
+    }
+    public void setState(Boolean state) {
+        this.state = state;
+    }
 
     @Contract(pure = true)
     Integer getNumber() {
@@ -64,8 +104,8 @@ final class Philosopher {
         this.name = name;
     }
 
-    @Contract(pure = true)
-    Boolean isState(Integer oneFork, Integer twoFork) {return state.isReady(oneFork, twoFork);}
+//    @Contract(pure = true)
+//    Boolean isState(Integer oneFork, Integer twoFork) {return state.isReady(oneFork, twoFork);}
 
     @Contract(pure = true)
     private static SimpleDateFormat getFormatDate() {
